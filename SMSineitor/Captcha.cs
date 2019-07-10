09caa88f9d752ecb6491650048e81772e677d666
AnotherPianist <cafarimo@outlook.com>
2019-07-10 11:26:04 -0400

    Added Captcha and Mensaje Classes

---
+++ SMSineitor/Captcha.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMSineitor
+{
+    class Captcha
+    {
+        public int a { get; set; }
+        public int b { get; set; }
+
+        public Captcha()
+        {
+                       
+        }
+
+        public string Generate()
+        {
+            Random rnd = new Random();
+            a = rnd.Next(100);
+            b = rnd.Next(100);
+            return base.ToString();
+        }
+
+        public bool Validate(int c) => a+b==c;
+        public override string ToString()
+        {
+            return Convert.ToString(a) + " + " + Convert.ToString(b);
+        }
+    }
+}
---
+++ SMSineitor/Mensaje.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMSineitor
+{
+    class Mensaje
+    {
+        int numero { get; set; }
+        string mensaje { get; set; }
+
+        Mensaje(int _numero, string _mensaje)
+        {
+            if (_numero >= 1000000000) throw new Exception("Número no válido");
+            if(_mensaje.Length > 50) throw new Exception("Mensaje no válido");
+            numero = _numero;
+            mensaje = _mensaje;
+        }
+
+        public override string ToString()
+        {
+            return mensaje;
+        }
+
+    }
+}
